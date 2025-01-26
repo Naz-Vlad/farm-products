7de7070381f0bf1a4fbcab7f1bb031a69caf02d4
Владимир <10nva63@gmail.com>
2025-01-26 16:13:46 +0300

    сделал копаненты: MainPage и Header

---
+++ proect_vite/src/components/header/header.jsx
@@ -0,0 +1,10 @@
+
+function Header() {
+	return (
+		<div>
+			<header>ШАПКА</header>
+		</div>
+	)
+}
+
+export default Header
---
+++ proect_vite/src/components/layout/page-wrpapeer/page-wrpapeer.jsx
@@ -0,0 +1,13 @@
+import Header from "../../header/header"
+import MainPage from "../main/main-page"
+
+function PageWrpapeer() {
+	return (
+    <>
+		  <Header/>
+      <MainPage/>
+    </>
+	)
+}
+
+export default PageWrpapeer
