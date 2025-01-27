1855ceede67fc55994c37e553b73f851625d9f3f
Владимир <10nva63@gmail.com>
2025-01-27 09:20:00 +0300

    сделал footer

---
+++ proect_vite/src/components/layout/footer/footer.jsx
@@ -0,0 +1,13 @@
+import Logo from '/src/components/ul/logo/logo'
+import './footer.css'
+
+function Footer() {
+	return (
+		<footer className='footer'>
+			<Logo/>
+			<span className="footer__copyright">Создано 2021</span>
+		</footer>
+	)
+}
+
+export default Footer
--- proect_vite/src/components/layout/page-wrpapeer/page-wrpapeer.jsx
+++ proect_vite/src/components/layout/page-wrpapeer/page-wrpapeer.jsx
@@ -1,12 +1,14 @@
 import Header from "/src/components/layout/header/header"
 import MainPage from "../main/main-page"
+import Footer from "../footer/footer"
 
 function PageWrpapeer() {
 	return (
-    <>
+		<>
 		  <Header/>
       <MainPage/>
-    </>
+			<Footer/>
+		</>
 	)
 }
 
